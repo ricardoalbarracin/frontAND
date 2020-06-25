472d7905de789fbcea211ef7f6dcb58ad9836617
diegoalcibar <diegoalcas@gmail.com>
2020-06-24 19:23:59 -0500

    Ajustes incidencias reportadas por el Ing Mario Nova, minsalud

--- src/src/app/tramites/minsalud-consulta/components/seguridad/seguridad-filtro/seguridad-filtro.component.ts
+++ src/src/app/tramites/minsalud-consulta/components/seguridad/seguridad-filtro/seguridad-filtro.component.ts
@@ -104,10 +104,10 @@ export class SeguridadFiltroComponent implements OnInit {
       }
     });
 
-    // Validación: Cantidad minima de filtros = 1
-    if (filterNumber > 0) {
+    // Validación: Cantidad minima de filtros = 1; >= 0 para quitar restriccion
+    if (filterNumber >= 0) {
       this.concultaService.invalidForm = false;
-      this.concultaService.getDetalle(this.concultaService.tipoDetalle.sanciones).subscribe(
+      this.concultaService.getDetalle(this.concultaService.tipoDetalle.seguridad).subscribe(
         (data) => this.success(data),
         (error) => {
           this.concultaService.invalidForm = true;
